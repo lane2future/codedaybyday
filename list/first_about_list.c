a518535913c556ff90962b2f8e76d6f38e8d8867
lane2future <lane_switcha@163.com>
2022-01-07 16:27:07 +0800

    first list func

---
+++ list/first_about_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+}node;
+
+int main(void)
+{
+    int i = 0;
+    node n1 = {1}, n2 = {2}, n3 = {3};
+    n1.next = &n2;
+    n2.next = &n3;
+    node *p = &n1;
+
+    for(i = 0; i < 3; i++)
+    {
+        printf("%d ",p->data);
+        p = p->next;
+    }
+    printf("\r\n");
+
+    return 0;}
